118d728f4ba916ce57a27a27744b51d468379946
s_anno <amo.20325@gmail.com>
2021-09-03 19:40:56 +0900

    feat(hello.ts): ignore no-unused-vars
    this function is used as entrypoint

--- src/hello.ts
+++ src/hello.ts
@@ -2,6 +2,7 @@ const greeter = (person: string) => {
   return `Hello, ${person}!`;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function testGreeter() {
   const user = "Grant";
   Logger.log(greeter(user));
