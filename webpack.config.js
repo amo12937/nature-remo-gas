87dd6cd0cc28025e461ee65a2018d58629c99510
s_anno <amo.20325@gmail.com>
2021-09-11 15:20:56 +0900

    feat(webpack): add webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const GasPlugin = require("gas-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  context: __dirname,
+  entry: "./src/index.ts",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+  },
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        enforce: "pre",
+        test: /\.[tj]s$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+    ],
+  },
+  plugins: [new GasPlugin()],
+};
