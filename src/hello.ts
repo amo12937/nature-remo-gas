b81168bb7af87a83c43fd6c6e2d0462324739224
s_anno <amo.20325@gmail.com>
2021-09-11 15:26:13 +0900

    style: follow lint

--- src/hello.ts
+++ src/hello.ts
@@ -1,13 +1,14 @@
 import retry from "async-retry";
 
-export const greeter = async (person: string): Promise<string> => await retry(
-  async (): string => {
-    return `Hello, ${person}!`;
-  },
-  {
-    retries: 5,
-  }
-);
+export const greeter = async (person: string): Promise<string> =>
+  await retry(
+    async (): string => {
+      return `Hello, ${person}!`;
+    },
+    {
+      retries: 5,
+    }
+  );
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function testGreeter(): void {
