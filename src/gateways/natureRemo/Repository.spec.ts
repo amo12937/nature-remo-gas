9879fa1edf28e4038ce3bef9b6d41f9c2ce5fb0e
s_anno <amo.20325@gmail.com>
2021-09-12 15:49:31 +0900

    test(nature remo): add test of repository

--- src/gateways/natureRemo/Repository.spec.ts
+++ src/gateways/natureRemo/Repository.spec.ts
@@ -6,7 +6,7 @@ import { Repository } from "@/gateways/natureRemo/Repository";
 
 describe(Repository.name, () => {
   describe("getDevices", () => {
-    it("should return the empty list when natureRemo return the empty list", () => {
+    it("should return the empty list when natureRemo returns the empty list", () => {
       const config: Config = {
         accessToken: "dummy",
         baseUrl: "dummy",
@@ -22,6 +22,75 @@ describe(Repository.name, () => {
       const actual = target.getDevices(currentDatetime);
 
       const expected: Device[] = [];
+      expect(actual).toEqual(expected);
+    });
+
+    it("should throw error when natureRemo returns non json value", () => {
+      const config: Config = {
+        accessToken: "dummy",
+        baseUrl: "dummy",
+      };
+      const fetcher: Fetcher = {
+        fetch: jest.fn().mockReturnValueOnce({
+          getContentText: jest.fn().mockReturnValueOnce("hoge"),
+        }),
+      };
+      const target = new Repository(config, fetcher);
+
+      const currentDatetime = DateTime.now();
+      expect((): void => {
+        target.getDevices(currentDatetime);
+      }).toThrowError();
+    });
+
+    it("should return Device list when natureRemo returns device list", () => {
+      const config: Config = {
+        accessToken: "dummy",
+        baseUrl: "dummy",
+      };
+      const fetcher: Fetcher = {
+        fetch: jest.fn().mockReturnValueOnce({
+          getContentText: jest.fn().mockReturnValueOnce(
+            JSON.stringify([
+              {
+                id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                name: "string",
+                temperature_offset: 0,
+                humidity_offset: 0,
+                created_at: "2021-09-12T06:44:50.710Z",
+                updated_at: "2021-09-12T06:44:50.710Z",
+                firmware_version: "string",
+                mac_address: "string",
+                serial_number: "string",
+                newest_events: {
+                  te: {
+                    val: 0,
+                    created_at: "2020-09-10T06:03:58.213Z",
+                  },
+                  hu: {
+                    val: 0,
+                    created_at: "2020-09-10T06:03:58.213Z",
+                  },
+                  il: {
+                    val: 0,
+                    created_at: "2020-09-10T06:03:58.213Z",
+                  },
+                  mo: {
+                    val: 1,
+                    created_at: "2020-09-10T06:03:58.213Z",
+                  },
+                },
+              },
+            ])
+          ),
+        }),
+      };
+      const target = new Repository(config, fetcher);
+
+      const currentDatetime = DateTime.now();
+      const actual = target.getDevices(currentDatetime).length;
+
+      const expected = 1;
       expect(actual).toEqual(expected);
     });
   });
