ee33be6beacbfbe12f664059c50bdfc278726e61
s_anno <amo.20325@gmail.com>
2021-09-18 18:14:22 +0900

    fix(bigquery): ignore any

--- src/entities/googleAppsScript/apis/bigquery/collection/JobsCollection.ts
+++ src/entities/googleAppsScript/apis/bigquery/collection/JobsCollection.ts
@@ -1,5 +1,6 @@
 import { Job } from "@/entities/googleAppsScript/apis/bigquery/schema/Job";
 
 export interface JobsCollection {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   insert(resource: Job, projectId: string, mediaData: any): Job;
 }
